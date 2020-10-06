bf85e535b57705018ca7658291cff5c2f377b4b2
Gurkirat Khaira <777gurkirat@gmail.com>
2020-10-06 19:05:44 -0400

    decending order update

--- src/Exercise3/SortValues.java
+++ src/Exercise3/SortValues.java
@@ -2,19 +2,26 @@ package Exercise3;
 
 public class SortValues {
 	static void sortNumber(int... numbers) {
-		int n = numbers.length;
 
-		for (int i = 0; i < n - 1; i++) {
-			int start = i;
-			for (int j = i + 1; j < n; j++)
-				if (numbers[j] < numbers[start])
-					start = j;
-
-			int next = numbers[start];
-			numbers[start] = numbers[i];
-			numbers[i] = next;
+		for (int i = 1; i < numbers.length; i++) {
+			for (int j = i; j > 0; j--) {
+				int temp;
+				if (numbers[j] < numbers[j - 1]) {
+					temp = numbers[j];
+					numbers[j] = numbers[j - 1];
+					numbers[j - 1] = temp;
+				}
+			}
 		}
-		for (int i = 0; i < n; i++) {
+		int last = numbers.length - 1;
+		int middle = numbers.length / 2;
+		for (int i = 0; i <= middle; i++) {
+			int temp = numbers[i];
+			numbers[i] = numbers[last - i];
+			numbers[last - i] = temp;
+		}
+
+		for (int i = 0; i < numbers.length; i++) {
 			System.out.print(numbers[i] + " ");
 		}
 	}
